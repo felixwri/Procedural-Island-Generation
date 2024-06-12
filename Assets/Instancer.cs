a49bf1253b40a7d7c930d424a3393f99613d2927
felixwri <73856380+felixwri@users.noreply.github.com>
2024-06-12 13:00:16 +0100

    Instancer refactor for readability

--- Assets/Instancer.cs
+++ Assets/Instancer.cs
@@ -1,71 +1,91 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
+
+public class InstancedPrefab
+{
+    private MeshFilter filter;
+    private MeshRenderer renderer;
+    private List<Matrix4x4> matrices = new List<Matrix4x4>();
+
+    public void Init(GameObject prefab)
+    {
+        filter = prefab.GetComponent<MeshFilter>();
+        renderer = prefab.GetComponent<MeshRenderer>();
+
+        for (int i = 0; i < renderer.sharedMaterials.Length; i++)
+        {
+            renderer.sharedMaterials[i].enableInstancing = true;
+        }
+    }
+
+    public void Add(Vector3 position, Quaternion rotation, Vector3 scale)
+    {
+        matrices.Add(Matrix4x4.TRS(position, rotation, scale));
+    }
+
+    public void Clear()
+    {
+        matrices.Clear();
+    }
+
+    public int Count()
+    {
+        return matrices.Count;
+    }
+
+    public void Update()
+    {
+        for (int i = 0; i < renderer.sharedMaterials.Length; i++)
+        {
+            Graphics.DrawMeshInstanced(filter.sharedMesh, i, renderer.sharedMaterials[i], matrices);
+        }
+    }
+}
+
 
 public class Instancer : MonoBehaviour
 {
     public GameObject treePrefab;
     public GameObject bushPrefab;
 
-    List<Matrix4x4> treeMatrices = new List<Matrix4x4>();
-    List<Matrix4x4> bushMatrices = new List<Matrix4x4>();
-
-    private MeshFilter treeMeshFilter;
-    private MeshRenderer treeMeshRenderer;
-    private MeshFilter bushMeshFilter;
-    private MeshRenderer bushMeshRenderer;
+    private readonly InstancedPrefab tree = new();
+    private readonly InstancedPrefab bush = new();
 
     // Start is called before the first frame update
     void Start()
     {
-
-        treeMeshFilter = treePrefab.GetComponent<MeshFilter>();
-        treeMeshRenderer = treePrefab.GetComponent<MeshRenderer>();
-        bushMeshFilter = bushPrefab.GetComponent<MeshFilter>();
-        bushMeshRenderer = bushPrefab.GetComponent<MeshRenderer>();
-
-
-        treeMeshRenderer.sharedMaterial.enableInstancing = true;
-
-        for (int i = 0; i < treeMeshRenderer.sharedMaterials.Length; i++)
-        {
-            treeMeshRenderer.sharedMaterials[i].enableInstancing = true;
-        }
-
-        bushMeshRenderer.sharedMaterial.enableInstancing = true;
-
+        tree.Init(treePrefab);
+        bush.Init(bushPrefab);
     }
 
     public void AddTree(Vector3 position, Quaternion rotation, Vector3 scale)
     {
-        treeMatrices.Add(Matrix4x4.TRS(position, rotation, scale));
+        tree.Add(position, rotation, scale);
     }
 
     public void AddBush(Vector3 position, Quaternion rotation, Vector3 scale)
     {
-        bushMatrices.Add(Matrix4x4.TRS(position, rotation, scale));
+        bush.Add(position, rotation, scale);
     }
 
     public void Clear()
     {
         Debug.Log("Clearing martices");
-        treeMatrices.Clear();
-        bushMatrices.Clear();
+        tree.Clear();
+        bush.Clear();
     }
 
     public void Log()
     {
-        Debug.Log("Tree Matrices: " + treeMatrices.Count);
-        Debug.Log("Bush Matrices: " + bushMatrices.Count);
+        Debug.Log("Tree Matrices: " + tree.Count());
+        Debug.Log("Bush Matrices: " + bush.Count());
     }
 
     void Update()
     {
-        for (int i = 0; i < treeMeshRenderer.sharedMaterials.Length; i++)
-        {
-            Graphics.DrawMeshInstanced(treeMeshFilter.sharedMesh, i, treeMeshRenderer.sharedMaterials[i], treeMatrices);
-        }
-
-        Graphics.DrawMeshInstanced(bushMeshFilter.sharedMesh, 0, bushMeshRenderer.sharedMaterial, bushMatrices);
+        tree.Update();
+        bush.Update();
     }
 }
